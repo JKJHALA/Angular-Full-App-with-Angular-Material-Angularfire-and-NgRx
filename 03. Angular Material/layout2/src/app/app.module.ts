568d991cc212e0673ee9d77f7cb68161cdce9f39
JKJHALA <JKJHALA@HOTMAIL.COM>
2021-11-15 15:16:14 +0530

    reactive validation

--- 03. Angular Material/layout2/src/app/app.module.ts
+++ 03. Angular Material/layout2/src/app/app.module.ts
@@ -6,18 +6,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MaterialModule } from './material.module';
 import { FormsModule } from '@angular/forms';
-
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     MaterialModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
 
   ],
   providers: [],
