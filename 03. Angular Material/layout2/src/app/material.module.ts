b2f9b53092b4fa5d00621f552a60e7f62081f428
JKJHALA <JKJHALA@HOTMAIL.COM>
2021-11-21 18:12:13 +0530

    navbar and starting toolbar

--- 03. Angular Material/layout2/src/app/material.module.ts
+++ 03. Angular Material/layout2/src/app/material.module.ts
@@ -3,11 +3,15 @@ import{MatButtonModule} from '@angular/material/button'
 import {MatInputModule} from '@angular/material/input'
 
 import { MatFormFieldModule } from "@angular/material/form-field";
+import {MatSidenavModule} from "@angular/material/sidenav"
 import { FlexLayoutModule } from "@angular/flex-layout";
+import {MatListModule} from "@angular/material/list";
+import {MatIconModule} from "@angular/material/icon";
+import {MatToolbarModule} from '@angular/material/toolbar'
 
 
 @NgModule({
-    imports:[MatButtonModule ,MatInputModule, MatFormFieldModule , FlexLayoutModule ],
-    exports:[MatButtonModule,MatInputModule,MatFormFieldModule,FlexLayoutModule]
+    imports:[MatButtonModule ,MatInputModule, MatFormFieldModule , FlexLayoutModule, MatSidenavModule ,MatListModule ,MatIconModule,MatToolbarModule],
+    exports:[MatButtonModule,MatInputModule,MatFormFieldModule,FlexLayoutModule,     MatSidenavModule, MatListModule,MatIconModule,MatToolbarModule]
 })
-export class MaterialModule{}
+export class MaterialModule{}MatSidenavModule
