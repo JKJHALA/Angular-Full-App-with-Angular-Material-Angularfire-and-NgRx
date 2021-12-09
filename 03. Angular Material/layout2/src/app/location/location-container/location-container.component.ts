07a46e9295b1c40fdb9954204be87e0c3398ca8b
JKJHALA <JKJHALA@HOTMAIL.COM>
2021-12-09 14:06:14 +0530

    single object selected

--- 03. Angular Material/layout2/src/app/location/location-container/location-container.component.ts
+++ 03. Angular Material/layout2/src/app/location/location-container/location-container.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ActivatedRouteSnapshot, Router } from '@angular/router';
+import { select, Store } from '@ngrx/store';
+import { AppState } from 'app/reducers';
+import { filter, first, map, Observable, of } from 'rxjs';
+import { LocationEntityService } from '../services/location-entity.service';
+import { Location } from '../model/location'
+import { selectLocationlById } from '../location-selector';
+import { LocationAdeptor } from '../locationAdeptor';
+
+
+
 
 @Component({
   selector: 'app-location-container',
@@ -7,9 +18,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocationContainerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router,
+    private activeRoute: ActivatedRoute,
+    private locationService: LocationEntityService,
+    private store: Store<AppState>,
+    private locationAdeptor: LocationAdeptor) { }
+
+  private locationId: string = ''
+  //private currentLocation$: Observable<Location | undefined> | undefined;
+
 
   ngOnInit(): void {
+
+
+    this.locationId = '631535';
+
+    this.locationAdeptor.currentLocation$ =this.locationService.collection$.pipe(
+      select(selectLocationlById(this.locationId)))
+
+
+
   }
+
+
 
 }
