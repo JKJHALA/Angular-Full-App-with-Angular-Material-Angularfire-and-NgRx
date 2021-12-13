8cd38d05bb2de68a23c1e4282b5a29380ed3c9a3
JKJHALA <JKJHALA@HOTMAIL.COM>
2021-12-13 19:49:14 +0530

    implementing update

--- 03. Angular Material/layout2/src/app/location/services/location-data.services.ts
+++ 03. Angular Material/layout2/src/app/location/services/location-data.services.ts
@@ -1,65 +1,90 @@
-import { HttpClient, HttpParams } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { DefaultDataService, HttpMethods, HttpUrlGenerator, QueryParams } from "@ngrx/data";
-import { client } from "app/auth/auth.selectors";
-import { map, Observable } from "rxjs";
-import { Location } from "../model/location";
+
+
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {
+  DefaultDataService,
+  HttpMethods,
+  HttpUrlGenerator,
+  QueryParams,
+} from '@ngrx/data';
+import { Update } from '@ngrx/entity';
+
+import { map, Observable, of } from 'rxjs';
+import { Location } from '../model/location';
 
 @Injectable()
-export class LocationDataService extends DefaultDataService<Location>{
-
-  domainUrl: string = "https://devwtcustomer.tmssaas.com/";
+export class LocationDataService extends DefaultDataService<Location> {
+  domainUrl: string = 'https://devwtcustomer.tmssaas.com/';
 
   constructor(http: HttpClient, httpUrlGenerator: HttpUrlGenerator) {
-    super("Location", http, httpUrlGenerator);
+    super('Location', http, httpUrlGenerator);
   }
 
+  override getAll(): Observable<Location[]> {
 
 
+    //   https://devwtcustomer.tmssaas.com/Services/Wcf/LocationService.svc/json/GetLocationsByClient
+    // const locUrl = this.domainUrl + 'Services/MasLocationService.svc/json/GetLocationByType';
 
-    override getAll(): Observable<Location[]> {
-   //   https://devwtcustomer.tmssaas.com/Services/Wcf/LocationService.svc/json/GetLocationsByClient
-   // const locUrl = this.domainUrl + 'Services/MasLocationService.svc/json/GetLocationByType';
-
-    const locUrl = this.domainUrl + 'Services/Wcf/LocationService.svc/json/GetPagedLocationForClient';
-
+    const locUrl =
+      this.domainUrl +
+      'Services/Wcf/LocationService.svc/json/GetPagedLocationForClient';
 
     let params = new HttpParams();
-    params = params.set('clientID', "1132")
-    .append('pageNo',1)
-    .append('recordCount',10)
-    .append('LocationTypeID', '3')
-    .append('LocationName', "abc")
+    params = params
+      .set('clientID', '1132')
+      .append('pageNo', 1)
+      .append('recordCount', 10)
+      .append('LocationTypeID', '3')
+      .append('LocationName', 'abc');
 
     const options = { params };
 
-    return this.http.get(locUrl,options)
-      .pipe(
-        map(res => (res as locRes)["GetPagedLocationForClientResult"])
-      )
+    return this.http
+      .get(locUrl, options)
+      .pipe(map((res) => (res as locRes)['GetPagedLocationForClientResult']));
   }
 
   //just to demostrate any customer para /method can be used
-   override getWithQuery(queryParams: QueryParams | string) :Observable<Location[]>{
-    const locUrl = this.domainUrl + 'Services/Wcf/LocationService.svc/json/GetPagedLocationForClient?'+ queryParams ;
+  override getWithQuery(
+    queryParams: QueryParams | string
+  ): Observable<Location[]> {
+    const locUrl =
+      this.domainUrl +
+      'Services/Wcf/LocationService.svc/json/GetPagedLocationForClient?' +
+      queryParams;
 
 
 
-   // const httpOptions = { params }; //can use sprade operator and append to existing options
-    return this.http.get(locUrl) //can switch based on method name
-      .pipe(
-        map(res => (res as locRes)["GetPagedLocationForClientResult"])
-      )
-
+    // const httpOptions = { params }; //can use sprade operator and append to existing options
+    return this.http
+      .get(locUrl) //can switch based on method name
+      .pipe(map((res) => (res as locRes)['GetPagedLocationForClientResult']));
   }
 
+  override update(location: Update<Location>) :Observable<Location>{
 
+
+
+    const locUrl =
+      this.domainUrl +
+      'Services/Wcf/LocationService.svc/json/UpdateMAS_Location';
+
+    this.http.post(locUrl, JSON.stringify(location)).subscribe();
+
+
+
+
+
+   return of({...newLocation,...location.changes});
+
+  }
 
 
 }
 
-
 //wrokaround to ignore ts error
 interface locRes {
-  GetPagedLocationForClientResult: []
+  GetPagedLocationForClientResult: [];
 }
